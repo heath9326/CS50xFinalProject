e33eaa53d10a65ece0f9eeef74c3533730b71f16
Evgeniya Seminenko <heath9326@yandex.ru>
2023-01-30 23:46:56 +0300

    Cleaning up

--- FlaskDemo/app.py
+++ FlaskDemo/app.py
@@ -1,9 +1,6 @@
 import os
 
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
