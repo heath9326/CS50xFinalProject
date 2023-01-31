2999129074ba28d7fd21d238558f5860eaf4fd83
Evgeniya Seminenko <heath9326@yandex.ru>
2023-01-31 16:09:21 +0300

    Revert "Cleaning up"
    This reverts commit e33eaa53d10a65ece0f9eeef74c3533730b71f16.

---
+++ FlaskDemo/app.py
@@ -0,0 +1,14 @@
+import os
+
+from flask import Flask, flash, redirect, render_template, request, session
+
+
+# Configure application
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
