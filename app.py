6b8cd8d99e121160b525ed5515d805ed40366aea
Evgeniya Seminenko <heath9326@yandex.ru>
2023-02-01 18:37:38 +0300

    SASS working, Before connecting DB

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from flask import Flask, flash, redirect, render_template, request, session
 # Configure application
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("layout.html")
     
