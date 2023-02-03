89b37cfec9afa623ba51f4898295a4ca4fe46e5a
Evgeniya Seminenko <heath9326@yandex.ru>
2023-02-03 21:33:45 +0300

    Setting up new index.html

--- app.py
+++ app.py
@@ -23,27 +23,5 @@ def db_connection():
 
 @app.route("/")
 def index():
-    #Establish db connection:
-    conn = db_connection()
-    #Grab cursor:
-    cursor = conn.cursor()
-
-    #Example of creating a select through cursor
-    cursor = conn.execute("SELECT * FROM database")
-    #Now sort the selection into a dictionary
-    books = [
-        dict(id=row[0], author=row[1], language=row[2], title=row[3])
-        for row in cursor.fetchall()
-    ]
-    if books is not None:
-        return jsonify(books)
-
-    #Establish the query you want performed
-    sql = """INSERT INTO book (author, language, title)
-             VALUES (?, ?, ?)"""
-    cursor = cur.execute(sql, (new_autor, new_lang, new_title))    
-    conn.commit()
-    return f"Book with the id {cursor.lastrowid} created successfully", 201
-    
-    return render_template("layout.html")
+    return render_template("index.html")
     
