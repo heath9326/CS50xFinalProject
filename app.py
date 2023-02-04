fefd7d53b75557e4a8e1a416e9dd42fc5f025f4e
Evgeniya Seminenko <heath9326@yandex.ru>
2023-02-04 17:45:18 +0300

    Yearly Budget Table ready, Buttons don't work yet

--- app.py
+++ app.py
@@ -21,7 +21,22 @@ def db_connection():
     return conn
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    # Variables, lists and dicts:
+    months = [{'name': 'January', 'number': '1'}, 
+                {'name': 'February', 'number': '2'},
+                {'name': 'March', 'number': '3'},
+                {'name': 'April', 'number': '4'}, 
+                {'name': 'May', 'number': '5'},
+                {'name': 'June', 'number': '6'},
+                {'name': 'July', 'number': '7'},
+                {'name': 'August', 'number': '8'},
+                {'name': 'September', 'number': '9'},
+                {'name': 'October', 'number': '10'},
+                {'name': 'November', 'number': '11'},
+                {'name': 'December', 'number': '12'}]
+    row_count = 1
+
+    return render_template("index.html", months=months, row_count=row_count)
     
